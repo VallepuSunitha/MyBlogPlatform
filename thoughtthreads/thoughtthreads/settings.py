8ba93f6b3b3ec18401e1caabb7e521250567bc03
Vallepu Sunitha <vallepusunitha248@gmail.com>
2026-03-24 13:09:04 +0530

    modified

--- thoughtthreads/thoughtthreads/settings.py
+++ thoughtthreads/thoughtthreads/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = os.environ.get('DEBUG') == 'True'
 
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://myblogplatform.onrender.com']
 
 
 # Application definition
